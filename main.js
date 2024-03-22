806d1591a01d3d616ef6a3f8b32125f01a86c5f9
Tiffany Huang <goldentogawhale1258@gmail.com>
2024-03-22 15:21:06 -0400

    feat: first try w wishbone chair

---
+++ main.js
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const loader = new GLTFLoader();
+
+loader.load(
+  "/chair.glb",
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+const directionalLight = new THREE.AmbientLight(0xffffff, 2);
+scene.add(directionalLight);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.z = 5;
+//camera.position.set(0, 20, 100);
+//controls.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  // /controls.update();
+  renderer.render(scene, camera);
+}
+animate();
